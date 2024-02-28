7d0c2f2330cf61eb87bc35c67dbd9199d0f59166
Nikki T <nicenikkituller@gmail.com>
2024-02-28 17:54:13 -0500

    Intake cmds!

--- src/main/java/frc/robot/Subsystems/Intake.java
+++ src/main/java/frc/robot/Subsystems/Intake.java
@@ -3,18 +3,30 @@ package frc.robot.Subsystems;
 import java.util.function.BooleanSupplier;
 import java.util.function.Supplier;
 
+import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import edu.wpi.first.units.Angle;
+import edu.wpi.first.units.Measure;
+import edu.wpi.first.units.Velocity;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import static edu.wpi.first.units.Units.Minute;
+import static edu.wpi.first.units.Units.Rotations;
+import static edu.wpi.first.units.Units.Second;
+
 public class Intake extends SubsystemBase{
     //2 Falcons --> Intake
+    public Timer m_timer;
     public TalonFX m_frontIntakeMotor;
     public TalonFX m_rearIntakeMotor;
     public DigitalInput m_intakeBeamBreaker;
     private boolean isStowed;
+     private final VelocityVoltage m_request = new VelocityVoltage(0);
+     double frontWheelTargetSpeed;
 
     public Intake(){
         m_intakeBeamBreaker = new DigitalInput(0);
@@ -28,13 +40,34 @@ public class Intake extends SubsystemBase{
 
     // ????????? Genuinely the worst thing I've ever seen
     // public Command stowedInSled(Supplier<Boolean> isStowed){
-    //     return runOnce(() -> {isStowed.equals(isInSled());});
+    //     return runOnce(() -> {isStowed.equals(isIn
+        // Sled());});
     // }
 
+    public Command toWheelSpeeds(Supplier<Measure<Velocity<Angle>>> vel){
+        // frontWheelTargetSpeed = velocity.in(Rotations.per(Minute));
+        return runEnd(() -> {
+            var velocity = vel.get();
+            m_frontIntakeMotor.setControl(m_request.withVelocity(velocity.in(Rotations.per(Second))));
+            m_rearIntakeMotor.setControl(m_request.withVelocity(velocity.in(Rotations.per(Second))));
+        }, () -> {
+            m_frontIntakeMotor.setControl(m_request.withVelocity(0));
+            m_rearIntakeMotor.setControl(m_request.withVelocity(0));
+        });
+    }
     public Command intake(){
         BooleanSupplier weAreStowed = () -> isInSled();
-        return run(null).until(weAreStowed);}
+        return runOnce(() -> {toWheelSpeeds(() -> Rotations.per(Minute).of(7000));}).until(weAreStowed);
+    }
+
+    public Command reject(){
+        return runOnce(
+            () -> {toWheelSpeeds(() -> Rotations.per(Minute).of(-7000))
+            .until(() -> {
+                return m_timer.hasElapsed(1);} //idk how much time
+            );
+        });
     }
     //Intake piece cmd
     //Reject piece cmd
-
+}
